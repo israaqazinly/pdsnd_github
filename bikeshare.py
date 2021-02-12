0d1bacb062ccf642c1b486493770f1341634fd89
Israa Qazinly <israaqaz@gmail.com>
2021-02-12 09:57:59 +0300

    Renamed ask_view to ask_user

--- bikeshare.py
+++ bikeshare.py
@@ -182,7 +182,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-def ask_view(df):
+def ask_user(df):
     view_data = input('\nWould you like to view 5 rows of individual trip data? Enter yes or no\n')
     start_loc = 0
     while (view_data != 'no'):
@@ -201,7 +201,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        ask_view(df)
+        ask_user(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
